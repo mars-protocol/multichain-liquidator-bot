3f11c4ffa05beac5ca8cb617ddc9961f093a8138
Donovan Solms <donovan.solms@gmail.com>
2022-08-26 09:18:13 +0200

    Add interface for basic deployer

--- manager/src/deployer/docker.go
+++ manager/src/deployer/docker.go
@@ -1,5 +1,53 @@
 package deployer
 
+import "github.com/sirupsen/logrus"
+
 // Docker implements a deployment mechanism using Docker as the orchestrator
 type Docker struct {
+	container string
+
+	isDeploying bool
+
+	logger *logrus.Entry
+}
+
+// NewDocker creates a new instance of the Docker deployer that will deploy
+// the given container
+func NewDocker(container string, logger *logrus.Entry) (*Docker, error) {
+	return &Docker{
+		container: container,
+		logger: logger.WithFields(logrus.Fields{
+			"subservice": "deployer",
+			"type":       "docker",
+		}),
+	}, nil
+}
+
+// Increase the number of services deployed
+func (dep *Docker) Increase() error {
+	dep.isDeploying = true
+	return nil
+}
+
+// Decrease the number of services deployed
+func (dep *Docker) Decrease() error {
+	dep.isDeploying = true
+	return nil
+}
+
+// RemoveAll removes all the services from deployment
+func (dep *Docker) RemoveAll() error {
+	dep.isDeploying = true
+	return nil
+}
+
+// Count returns the amount of services deployed
+func (dep *Docker) Count() (int, error) {
+	return 0, nil
+}
+
+// IsDeploying returns true while an increase or decrease of services have
+// been requested but not completed yet
+func (dep *Docker) IsDeploying() bool {
+	return dep.isDeploying
 }
--- manager/src/interfaces/deployer.go
+++ manager/src/interfaces/deployer.go
@@ -3,4 +3,15 @@ package interfaces
 // Deployer defines the behaviour of a component that handles deployment
 // of services
 type Deployer interface {
+	// Increase the number of services deployed
+	Increase() error
+	// Decrease the number of services deployed
+	Decrease() error
+	// RemoveAll removes all the services from deployment
+	RemoveAll() error
+	// Count returns the amount of services deployed
+	Count() (int, error)
+	// IsDeploying returns true while an increase or decrease of services have
+	// been requested but not completed yet
+	IsDeploying() bool
 }
