ec1c85b05eb90701f97a6f0f1f9e08eaacf86632
Mark Watney <josiah@delphilabs.io>
2022-09-05 21:17:33 +0800

    doc(liquidator): adding docs to liquidation generator

--- liquidator/liquidation_generator.ts
+++ liquidator/liquidation_generator.ts
@@ -2,6 +2,19 @@ import { Asset } from "./types/asset.js"
 import { Position } from "./types/position.js"
 import { LiquidationTx } from './types/liquidation.js'
 
+/**
+ * Generate a simple liquidation tx for a position.
+ * 
+ * Note that if a position has mutiple debts and collaterals, it will chose the largest of each by raw amount.
+ * This is a somewhat limited approach, as this may not reflect close to the USD value of the collateral
+ * or debt. While suboptimal, this is not unsafe as in the case of a smaller debt being repaid or collateral 
+ * claimed, we will just liquidate again in a couple blocks if it is still unhealthy. Redbank gracefully 
+ * handles situations where collateral requested has a value less than the debt being repaid, so this is also
+ * safe
+ * 
+ * @param position The unhealthy position to be liquidated
+ * @returns A liquidation transaction for the given position.
+ */
 export const createLiquidationTx = (position : Position) : LiquidationTx => {
     return {
         collateral_denom: getLargestCollateral(position.collaterals),
