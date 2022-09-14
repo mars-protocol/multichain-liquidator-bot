286ee44ba54b966cf6c9754f5fc3c384fbfa3dea
Donovan Solms <donovan.solms@gmail.com>
2022-09-14 10:31:41 +0200

    Add caching for metrics

---
+++ runtime/interfaces/cacher.go
@@ -0,0 +1,15 @@
+package interfaces
+
+// Cacher defines the available actions for caches
+type Cacher interface {
+	// Connect to the queue
+	Connect() error
+	// Set a float value at key
+	Set(key string, value interface{}) error
+	// Get an float value at key
+	GetFloat64(key string) (float64, error)
+	// Delete a key
+	Delete(key string) error
+	// Disconnect from the queue
+	Disconnect() error
+}
