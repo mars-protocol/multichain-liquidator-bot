3d35703e7fe7484b70c7b402d5c19f63b4b18770
Mark Watney <josiah@delphilabs.io>
2022-09-21 19:09:46 +0800

    refactor(liquidator): use osmosis repo for addresses, remove addresses.json

--- liquidator/src/helpers.ts
+++ liquidator/src/helpers.ts
@@ -8,15 +8,16 @@ export async function sleep(timeout: number) {
   }
   
 // Reads json containing contract addresses located in /artifacts folder for specified network.
-export function readAddresses() : ProtocolAddresses {
+export function readAddresses(deployConfigPath: string) : ProtocolAddresses {
+ 
   try {
     const data = readFileSync(
-      `addresses.json`,
+      deployConfigPath,
       "utf8"
     );
-    return JSON.parse(data);
+    return JSON.parse(data).addresses;
   } catch (e) {
-    console.error("Failed to load addresses.json - please ensure a valid address.json file is located under module root directory")
+    console.error(`Failed to load artifacts path - could not find ${deployConfigPath}`)
     process.exit(1)
   }
 }
