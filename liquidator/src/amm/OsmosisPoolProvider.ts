e6db4cfd324661143b6bc0b945a4f724994c5278
Mark Watney <josiah@delphilabs.io>
2023-06-07 14:29:42 +0800

    feat: update OsmosisPoolProvider to support loading all pools, not just Balancer types

--- liquidator/src/amm/OsmosisPoolProvider.ts
+++ liquidator/src/amm/OsmosisPoolProvider.ts
@@ -1,5 +1,5 @@
 import { camelCaseKeys } from "../helpers";
-import { Pagination, Pool } from "../types/Pool";
+import { ConcentratedLiquidityPool, Pagination, Pool, PoolType, XYKPool } from "../types/Pool";
 import { PoolDataProviderInterface } from "./PoolDataProviderInterface";
 
 export class OsmosisPoolProvider implements PoolDataProviderInterface {
@@ -7,37 +7,42 @@ export class OsmosisPoolProvider implements PoolDataProviderInterface {
     constructor(private lcdEndpoint: string) {}
 
     loadPools = async (): Promise<Pool[]> => {
-		let fetchedAllPools = false
-		let nextKey = ''
+		let fetched = false
 		let pools: Pool[] = []
-		let totalPoolCount = 0
-		while (!fetchedAllPools) {
-			const response = await fetch(
-				`${this.lcdEndpoint}/osmosis/gamm/v1beta1/pools${nextKey}`,
-			)
-			const responseJson: any = await response.json()
-			
-			if (responseJson.pagination === undefined) {
-				fetchedAllPools = true
-				return pools
-			}
+		let retryCount = 0
 
-			const pagination = camelCaseKeys(responseJson.pagination) as Pagination
-
-			// osmosis lcd query returns total pool count as 0 after page 1 (but returns the correct count on page 1), so we need to only set it once
-			if (totalPoolCount === 0) {
-				totalPoolCount = pagination.total
-			}
-
-			const poolsRaw = responseJson.pools as Pool[]
-
-			poolsRaw.forEach((pool) => pools.push(camelCaseKeys(pool) as Pool))
-
-			nextKey = `?pagination.key=${pagination.nextKey}`
-			if (pools.length >= totalPoolCount) {
-				fetchedAllPools = true
-			}
+		while (!fetched && retryCount < 5) {
+			try {
+				const response = await fetch(
+					`${this.lcdEndpoint}/osmosis/poolmanager/v1beta1/all-pools`,
+				)
+				const responseJson: any = await response.json()
+	
+				pools = responseJson.pools.map((data: any) => {
+					if (data['@type'] === '/osmosis.concentratedliquidity.v1beta1.Pool') {
+					  const result =  camelCaseKeys(data) as ConcentratedLiquidityPool;
+					  result.poolType = PoolType.CONCENTRATED_LIQUIDITY
+					  return result
+					} else if (data['@type'] === '/osmosis.gamm.v1beta1.Pool') {
+						const result = camelCaseKeys(data) as XYKPool
+						result.poolType = PoolType.XYK
+						result.token0 = result.poolAssets[0].token.denom
+						result.token1 = result.poolAssets[1].token.denom
+						return result
+					} else {
+					  // just skip, 
+					  console.log('unsupported pool type')
+					}
+				  });
+	
+				fetched = true
+			} catch {
+				retryCount++
+				console.log('retrying fetch')
+			}		
 		}
+
+		// fetch concentrated liquidity pools
 
 		return pools
 	}
--- liquidator/src/types/Pool.ts
+++ liquidator/src/types/Pool.ts
@@ -3,12 +3,35 @@ import { Coin } from '@cosmjs/amino'
 export interface PoolAsset {
 	token: Coin
 }
+export enum PoolType {
+	CONCENTRATED_LIQUIDITY = 'concentrated',
+	XYK = 'xyk',
+	STABLESWAP = 'stable',
+}
 
 export interface Pool {
 	address: string
 	id: Long
 	swapFee: string
+	poolType: PoolType
+	token0: string;
+	token1: string;
+}
+
+export interface XYKPool extends Pool {
 	poolAssets: PoolAsset[]
+}
+
+export interface ConcentratedLiquidityPool extends Pool { 
+	incentives_address: string;
+	spread_rewards_address: string;
+	current_tick_liquidity: string;
+	current_sqrt_price: string;
+	current_tick: string;
+	tick_spacing: string;
+	exponent_at_price_one: string;
+	spread_factor: string;
+	last_liquidity_update: string;
 }
 
 export interface Pagination {
