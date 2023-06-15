60359821b985836d76dff044c1c12239615b691b
Mark Watney <josiah@delphilabs.io>
2023-06-15 14:57:22 +0800

    feat: support routing for cl pools

--- liquidator/src/types/RouteHop.ts
+++ liquidator/src/types/RouteHop.ts
@@ -1,13 +1,8 @@
-import BigNumber from 'bignumber.js'
+import { Pool } from './Pool'
 
 export interface RouteHop {
 	poolId: Long
 	tokenInDenom: string
 	tokenOutDenom: string
-	// What percentage of swap is given to LP's + protocol, as a decimal. 0.3% = 0.003
-	swapFee: number
-
-	// number of assets in the pool. x is same denom as tokenIn, y is same denom as tokenOut
-	x1: BigNumber
-	y1: BigNumber
+	pool : Pool
 }
