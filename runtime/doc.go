1fec37eb4d4846a55697bea84396416b49628992
Mark Watney <mark@marsprotocol.foundation>
2024-06-24 15:44:07 +0800

    remove redundant files

--- runtime/doc.go
+++
@@ -1,3 +0,0 @@
-// Package runtime provides interfaces, consts, structs and implementations
-// for the bot's services
-package runtime
--- runtime/types/endpoints.go
+++
@@ -1,8 +0,0 @@
-package types
-
-// Endpoints denote the format for usable endpoints
-type Endpoints struct {
-	Hive string `json:"hive"`
-	LCD  string `json:"lcd"`
-	RPC  string `json:"rpc"`
-}
