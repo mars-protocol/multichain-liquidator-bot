d18ccbe17e0dbac877b76ba9e0adf727d98f0669
Mark Watney <mark@marsprotocol.foundation>
2024-11-14 18:19:49 +0800

    general tidying

--- src/query/routing/OsmosisRouteRequester.ts
+++ src/query/routing/OsmosisRouteRequester.ts
@@ -1,10 +1,12 @@
 import { RequestRouteResponse, RouteRequester } from './RouteRequesterInterface'
 
 export class OsmosisRouteRequester extends RouteRequester {
-	// @ts-ignore todo before deploying update on osmosis
 	requestRoute(
+		//@ts-ignore
 		tokenInDenom: string,
+		//@ts-ignore
 		tokenOutDenom: string,
+		//@ts-ignore
 		tokenInAmount: string,
 	): Promise<RequestRouteResponse> {
 		throw new Error('Method not implemented.')
