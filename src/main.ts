f1dc93cbf80d079e1684fe0bbaac2807dfefab9b
Mark Watney <josiah@delphilabs.io>
2025-01-16 14:34:05 +1000

    fix variable name

--- src/main.ts
+++ src/main.ts
@@ -67,7 +67,7 @@ export const main = async () => {
 	const routeRequester =
 		chainName === 'neutron'
 			? new AstroportRouteRequester(process.env.ASTROPORT_API_URL!)
-			: new OsmosisRouteRequester(process.env.SIDECAR_QUERY_SERVER!)
+			: new OsmosisRouteRequester(process.env.SQS_URL!)
 
 	switch (executorType) {
 		case REDBANK:
