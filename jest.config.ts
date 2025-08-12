205ad4d2d1b784b04634c783439d4cde40630d11
Subham <39151404+Subham2804@users.noreply.github.com>
2025-08-12 10:06:38 +0530

    Liquidation bot metrics (#132)
    * add metrics
    
    * add deploy workflow
    
    * update yaml and fix tests config
    
    * revert format check
    
    * temp branch
    
    * temp deploy
    
    * update deploy permission
    
    * update deploy permission
    
    * add deploy to v2
    
    * update metrics
    
    * update metrics with fix
    
    * fix lints and workflow
    
    format
    
    format
    
    update total successful metrics
    
    update total successful metrics
    
    update total successful metrics
    
    * fix comment for stable won metrics
    
    * format

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,21 @@ const config: Config = {
 	preset: 'ts-jest',
 	verbose: true,
 	testTimeout: 30000,
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.ts$': 'ts-jest',
+	},
+	moduleFileExtensions: ['ts', 'js', 'json'],
+	testPathIgnorePatterns: ['/node_modules/', '/build/'],
+	collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/types/**/*.ts'],
+	coverageDirectory: 'coverage',
+	coverageReporters: ['text', 'lcov', 'html'],
+	globals: {
+		'ts-jest': {
+			tsconfig: 'tsconfig.json',
+			isolatedModules: true,
+		},
+	},
 }
 
 export default config
